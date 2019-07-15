59dbc2e67b985425d9bebea58807eeb61b174fe6
matsuo <pagudonguri@gmail.com>
2019-07-15 14:33:13 +0900

    wip

--- open_pr_on_gh.rb
+++ open_pr_on_gh.rb
@@ -1,15 +1,22 @@
 # Open the PullRequest on Github
 #
 # Usage
-#   ruby open_pr_on_gh.rb <commit_hash>
+#   ruby open_pr_on_gh.rb <file_path> <line_number>
 #
 
-commit_hash = ARGV[0]
+file_path = ARGV[0]
+line_number = ARGV[1]
 
-merged_log = `git log --merges --oneline --reverse --ancestry-path #{commit_hash}...master`
-pr_number = merged_log.match(%r!Merge pull request #([0-9]*) !)[1]
+file_path = File.absolute_path(file_path)
+dir_path = File.dirname(file_path)
 
-git_remote_url = `git remote get-url origin`
+commit_hash = `cd #{dir_path};git blame -n -L #{line_number},#{line_number} #{file_path} | cut -d ' ' -f 1`
+puts commit_hash
+puts `cd #{dir_path};git log --merges --oneline --reverse --ancestry-path #{commit_hash}...feature/open_pr_fix_arg`
+exit
+pr_number = `cd #{dir_path};git log --merges --oneline --reverse --ancestry-path #{commit_hash}...master | grep 'Merge pull request #' | head -n 1 | cut -d ' ' -f 5 | sed -e 's/#//'`
+
+git_remote_url = `cd #{dir_path};git remote get-url origin`
 matched = git_remote_url.match(%r{git@github.com:(.*)/(.*).git})
 gh_user_name, gh_repo_name = matched[1], matched[2]
 
